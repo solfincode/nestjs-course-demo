955a967625c73910bfc178d5cf9d871982a3d977
duo <hypersonolabs@gmail.com>
2022-05-21 19:52:04 +0900

    hello world nestjs and create /class api route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/class')
+  getClass(): string {
+    return this.appService.getClass();
+  }
 }
